4452e61623cee7043434aae38be4b5241411d191
BirhanuGebisa <birhanugebisa@gmail.com>
2022-09-25 20:27:30 +0300

    update

--- airflow/dags/data_ingest.py
+++ airflow/dags/data_ingest.py
@@ -1,83 +1,93 @@
-import os
+from airflow import DAG 
+import pandas as pd
 import sys
-from datetime import datetime
+import os
+import pandas as pd
+import json
+print(os.path.abspath("working.............................."))
+sys.path.append(os.path.abspath("data"))
+from sqlalchemy import Numeric
+from datetime import datetime as dt
 from datetime import timedelta
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.email_operator import EmailOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+AIRFLOW_HOME = os.getenv('AIRFLOW_HOME')
 
-cwd=os.getcwd()
-sys.path.append(f'../sql_data/')
-sys.path.append(f'../temp/')
-sys.path.append(f'../scr/')
-sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
-from data_load import Extractor
-import file_load
-
-data_extractor=Extractor()
-
-def extract_data(ti):
-
-    loaded_df_name=data_extractor.extract_data(file_name='20181024_d1_0830_0900.csv',return_json=True)
-    trajectory_file_name,vehicle_file_name=loaded_df_name
-   
-    ti.xcom_push(key="trajectory",value=trajectory_file_name)
-    ti.xcom_push(key="vehicle",value=vehicle_file_name)
-
-def create_table():
-    file_load.create_table()
-
-def populate_table(ti):
-    trajectory_file_name = ti.xcom_pull(key="trajectory",task_ids='extract_from_file')
-    vehicle_file_name = ti.xcom_pull(key="vehicle",task_ids='extract_from_file')
-    # trajectory_data,vehicle_data=combined_df['trajectory'], combined_df['vehicle']
-    db_util.insert_to_table(trajectory_file_name, 'trajectories',from_file=True)
-    db_util.insert_to_table(vehicle_file_name, 'vehicles',from_file=True)
-
-def clear_memory(ti):
-    trajectory_file_name = ti.xcom_pull(key="trajectory",task_ids='extract_from_file')
-    vehicle_file_name = ti.xcom_pull(key="vehicle",task_ids='extract_from_file')
-
-    os.remove(f'../temp/{trajectory_file_name}')
-    os.remove(f'../temp/{vehicle_file_name}')
-
-# Specifing the default_args
+# defaults
 default_args = {
-    'owner': 'Birhanu',
+    'owner': 'birhanu',
     'depends_on_past': False,
     'email': ['birhanugebisa@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0,
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    'start_date': dt(2022, 9, 22),
     'retry_delay': timedelta(minutes=5)
 }
-
+ 
+#Airflow
 with DAG(
-    dag_id='extractor_loader_pg',
+    dag_id='Local_ELT_DAG',
     default_args=default_args,
-    description='this loads our data to the database',
-    start_date=datetime(2022,9,22,3),
-    schedule_interval='@daily',
-    catchup=False
-) as dag:
-    
-    read_data = PythonOperator(
-        task_id='extract_from_file',
-        python_callable = extract_data,
-    ) 
-    
-    create_tables = PythonOperator(
-        task_id='create_table',
-        python_callable = create_table
-    )
-    
-    populate_db = PythonOperator(
-        task_id='load_data',
-        python_callable = populate_table
-    ) 
+    description='Upload data from CSV to Postgres and Transform it with dbt',
+    schedule_interval="@daily",
+    start_date=days_ago(1),
+    dagrun_timeout=timedelta(minutes=60),
+) as pg_dag:
 
-    clear_temp_data = PythonOperator(
-        task_id='delete_temp_files',
-        python_callable = clear_memory
+ table_creator = PostgresOperator(
+    task_id="create_table", 
+    postgres_conn_id="postgres_server",
+    sql = '''
+        CREATE TABLE  IF NOT EXISTS pnuema( 
+            id serial primary key,
+            track_id numeric, 
+            type text not null, 
+            traveled_d double precision DEFAULT NULL,
+            avg_speed double precision DEFAULT NULL, 
+            lat double precision DEFAULT NULL, 
+            lon double precision DEFAULT NULL, 
+            speed double precision DEFAULT NULL,    
+            lon_acc double precision DEFAULT NULL, 
+            lat_acc double precision DEFAULT NULL, 
+            time double precision DEFAULT NULL
+        );
+    '''
     )
+data_loader = PostgresOperator(
+    sql="sql_data/data_load.sql",
+    task_id="load_data",
+    postgres_conn_id="postgres_server",
+)
+#transform data
+DBT_PROJECT_DIR = "/opt/airflow/dbt_demo"
+dbt_run = BashOperator(
+    task_id="dbt_run",
+    bash_command=f"cd ~/dbt_demo && ~/.local/bin/dbt run --profiles-dir {DBT_PROJECT_DIR}",
+ )
 
-    [read_data,create_tables]>>populate_db>>clear_temp_data
+dbt_test = BashOperator(
+    task_id="dbt_test",
+    bash_command=f"dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+ )
+
+dbt_doc_generate = BashOperator(
+    task_id="dbt_doc_generate", 
+    bash_command="dbt docs generate --profiles-dir /opt/airflow/dbt --project-dir "
+                    "/opt/airflow/dbt_demo"
+ )
+
+email_report = EmailOperator(
+    task_id='send_email',
+    to='birhanugebisa@gmail.com',
+    subject='Daily report generated',
+    html_content=""" <h3>Congratulations! ELT process is completed.</h3> """
+ )
+
+   #Task
+
+table_creator >> data_loader >> dbt_run >> dbt_test >> dbt_doc_generate >> email_report
